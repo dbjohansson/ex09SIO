6607a54e236e7205ba30a42e7c86190cc6464423
dbjohansson <denice.b.johansson1@gmail.com>
2015-03-24 19:29:40 +0100

    Update game.dart

--- web/game.dart
+++ web/game.dart
@@ -1,8 +1,13 @@
 // Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'dart:html';
+library bonhomme;
 
-void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+import 'dart:html';
+import 'package:game/game.dart';
+
+main() {
+  new Board(querySelector('#canvas'), new Trees(64)).draw();
 }
+
+
